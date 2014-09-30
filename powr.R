debeab8e544ddfb15f5956d7f5fa16ebe76a0cf7
Craig Ingram <cingram@weather.com>
2014-09-30 10:25:02 -0400

    using brewer to pick colors for the plot automagically. refactored the powr ranking to be a bit more generic

--- powr.R
+++ powr.R
@@ -1,4 +1,5 @@
 library("rjson")
+library("RColorBrewer")
 
 load <- function(url) {
   res <- readLines(url, warn = "F")
@@ -20,54 +21,56 @@ getGameResult <- function(team, matchup) {
   c(gmRes, score, oppScore)
 }
 
-powr <- function(js, numGames) {
-  powr0(js, numGames)
+powr <- function(js, week) {
+  powr0(js, week)
 }
 
-powrSeason <- function(js) {
+powrSeason <- function(js, numGames = 16) {
   myprks <- data.frame()
-  for (i in 1:16) {
-    print(sprintf("Week %d", i))
-    prks <- powr(js, i)
+  for (week in 1:numGames) {
+    print(sprintf("Week %d", week))
+    prks <- powr(js, week)
     print(prks[with(prks, order(-powerRanking)),])
     cols <- prks[["powerRanking"]]
     myprks <- rbind(myprks, cols)
-    if (i == 1)
-      colnames(myprks) <- prks[["team.teamAbbrev"]]
+    if (week == 1) colnames(myprks) <- prks[["team.teamAbbrev"]]
   }
   myprks
 }
 
 plotPowr <- function(prks) {
-  rng <- range(prks[3:16,])
+  numWeeks <- nrow(prks)
+  rng <- range(prks[3:numWeeks,])
   shps <- as.integer(runif(10, 21, 25))
-  clrs <- c("blue", "red", "forestgreen", "darkslateblue", "orange", "saddlebrown", "yellow3", "mediumpurple4", "olivedrab4", "hotpink2")
-  plot(prks[3:16,1], type = "o", col = clrs[[1]], pch = shps[[1]], axes = FALSE, ann = FALSE, ylim = rng)
+  clrs <- brewer.pal(ncol(prks), "Paired")
+  plot(prks[3:numWeeks,1], type = "o", col = clrs[[1]], pch = shps[[1]], axes = FALSE, ann = FALSE, ylim = rng)
   title(main = "Power Rankings by Week")
   title(xlab = "Week")
   title(ylab = "Power Ranking")
   axis(1, at = 1:14, labels = 3:16)
   axis(2)
   box()
-  for (p in 2:10) lines(prks[3:16,p], type = "o", col = clrs[[p]], pch = shps[[p]])
-  legend(1, rng[2], colnames(prks), col = clrs, cex = 0.5, pch = shps, lty = 1:2)
+  for (p in 2:10) lines(prks[3:numWeeks,p], type = "o", col = clrs[[p]], pch = shps[[p]])
+  legend(1.9, 135, colnames(prks), col = clrs, cex = 0.6, pch = shps, lty = 1:2)
 }
 
-calcPowr <- function(numGames, team, scores, oppScores, wlt) {
+calcPowr <- function(week, team, scores, oppScores, wlt) {
   tw <- wlt[[1]]
-  wp <- tw / numGames
+  wp <- tw / week
   mm <- unlist(range(scores))
-  tp <- sum(scores)
+  tpf <- sum(scores)
   tpa <- sum(oppScores)
-  avgScore <- tp / numGames
-  avgOppScore <- tpa / numGames
+  avgScore <- tpf / week
+  avgOppScore <- tpa / week
   minScore <- mm[[1]]
   maxScore <- mm[[2]]
   powerRanking <- ((avgScore * 6) + ((minScore + maxScore) * 2) + (wp * 400))/10
   wins <- wlt[[1]]
   losses <- wlt[[2]]
   ties <- wlt[[3]]
-  data.frame(team$teamAbbrev, team$teamId, tp, tpa, minScore, maxScore, wp, wins, losses, ties, avgScore, avgOppScore, powerRanking)
+  pf <- tail(scores, n = 1)
+  pa <- tail(oppScores, n = 1)
+  data.frame(team$teamAbbrev, team$teamId, pf, pa, tpf, tpa, minScore, maxScore, wp, wins, losses, ties, avgScore, avgOppScore, powerRanking)
 }
 
 powr0 <- function(js, numGames) {
@@ -89,3 +92,19 @@ powr0 <- function(js, numGames) {
   }
   powerRankings
 }
+
+breakdown <- function(prks) {
+  mypf <- data.frame()
+  for (i in 1:length(prks) + 1) {
+    print(sprintf("Week %d", i))
+    print(prks[with(prks, order(-pf)),])
+    cols <- prks[["pf"]]
+    mypf <- rbind(mypf, cols)
+    if (i == 1) colnames(mypf) <- prks[["team.teamAbbrev"]]
+  }
+  mypf
+}
+
+# js <- load("http://games.espn.go.com/ffl/api/v2/teams?leagueId=709331&seasonId=2014")
+# foo <- powrSeason(js, 3)
+# plotPowr(foo)
